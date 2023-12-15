1b2da6aee4e5c210de90803fe2a3481db3fec450
RYKT <rykt1024@163.com>
2023-12-15 14:46:11 +0800

    Add http basic server.

--- json.go
+++ json.go
@@ -54,3 +54,34 @@ func (g GradientConfig) toJSONString() (string, error) {
 	}
 	return string(jsonBytes), nil
 }
+
+// // 创建 BreathConfig 结构体实例
+// breathConfig := BreathConfig{
+// 	Onboard:    0,
+// 	Brightness: 0.2,
+// 	Speed:      0.3,
+// 	Color:      []int{255, 0, 120},
+// }
+
+// // 使用 toJSONString 方法将 BreathConfig 转换为 JSON 字符串
+// breathJSON, err := breathConfig.toJSONString()
+// if err != nil {
+// 	fmt.Println("Error converting BreathConfig to JSON:", err)
+// 	return
+// }
+
+// // 创建 GradientConfig 结构体实例
+// gradientConfig := GradientConfig{
+// 	Onboard:    0,
+// 	Brightness: 0.4,
+// 	Speed:      0.8,
+// 	ColorFrom:  []int{255, 0, 120},
+// 	ColorTo:    []int{0, 255, 0},
+// }
+
+// // 使用 toJSONString 方法将 GradientConfig 转换为 JSON 字符串
+// gradientJSON, err := gradientConfig.toJSONString()
+// if err != nil {
+// 	fmt.Println("Error converting GradientConfig to JSON:", err)
+// 	return
+// }
